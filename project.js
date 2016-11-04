7402a46b34b5dbbc88be3b56e93619838ff529d0
MeDaveD <medavedik@yandex.ru>
2016-11-04 03:02:22 +0300

    Edit package.json and add TDD

--- project.js
+++ project.js
@@ -1,19 +1,50 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+
 var app = express();
+
 var handlebars = require('express-handlebars')
-  .create({ defaultLaoyut: 'main'});
+  .create({ defaultLayout: 'main',
+  helpers: {
+              section: function (name, options) {
+                  if (!this._sections) {
+                      this._sections = {};
+                  }
+                  this._sections[name] = options.fn(this);
+                  return null;
+              }
+            }
+  });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
 app.use(express.static(__dirname + '/public'));
+
 app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production' &&
+  req.query.test === '1';
+  next();
+});
 
 app.get('/', function(req, res){
   res.render('home');
 });
 
 app.get('/about', function(req, res){
-  res.render('about', { fortune: fortune.getFortune });
+  res.render('about', {
+    fortune: fortune.getFortune (),
+    pageTestScript: '/qa/tests-about.js'
+  });
+});
+
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+  res.render('tours/request-group-rate');
 });
 
 app.use(function(req, res, next){
@@ -27,9 +58,9 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-
-
 app.listen(app.get('port'), function(){
 
   console.log('Express started on http://localhost:'+ app.get('port')+ 'push Crtl-C on exit...');
 });
+
+if( app.thing === null ) console.log('Бе-е!');
